7aae09615ddd26de53f815d6e6212599d364a933
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-08-06 22:45:50 -0600

    Fixed bug where time property type was string and not comparring to the current time

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,48 @@
 // object array holding date/time information for each timeblock in an 8-hour day
 var timeblockObjects = [
   {
-    hour: "9",
+    hour: 9,
+    milTime: 9,
     aORp: "AM",
   },
   {
-    hour: "10",
+    hour: 10,
+    milTime: 10,
     aORp: "AM",
   },
   {
-    hour: "11",
+    hour: 11,
+    milTime: 11,
     aORp: "AM",
   },
   {
-    hour: "12",
+    hour: 12,
+    milTime: 12,
     aORp: "PM",
   },
   {
-    hour: "1",
+    hour: 1,
+    milTime: 13,
     aORp: "PM",
   },
   {
-    hour: "2",
+    hour: 2,
+    milTime: 14,
     aORp: "PM",
   },
   {
-    hour: "3",
+    hour: 3,
+    milTime: 15,
     aORp: "PM",
   },
   {
-    hour: "4",
+    hour: 4,
+    milTime: 16,
     aORp: "PM",
   },
   {
-    hour: "5",
+    hour: 5,
+    milTime: 17,
     aORp: "PM",
   },
 ];
@@ -41,9 +50,9 @@ var timeblockObjects = [
 // JQUERY HTML REFERENCE VARIABLES
 var currentDay = $("#currentDay");
 
-// MOMENT VARIABLE DECLARATIONS
+// MOMENT GLOBAL VARIABLE DECLARATIONS
 var todaysDate = moment().format("dddd, MMMM Do YYYY");
-var currentTime = moment().format("hh:mm");
+var currentHour = moment().format("HH");
 
 // put today's date in the DOM header element
 currentDay.text(todaysDate);
@@ -57,7 +66,23 @@ $.each(timeblockObjects, function () {
 
   // time div elements
   var timeblockTime = $("<div>");
-  timeblockTime.text(this.hour);
-  timeblockTime.addClass("hour");
-  timeblockTime.append(this.hour);
+  timeblockTime.text(`${this.hour}${this.aORp}`);
+  timeblockTime.addClass("hour col-md-1");
+  timeblockContainer.append(timeblockTime);
+
+  // timeblock input element
+  var timeblockInput = $("<textarea>");
+  timeblockInput.addClass("description col-md-10");
+  timeblockContainer.append(timeblockInput);
+
+  //compare current time to current index and style text area element based on if current time is past, present, or future
+  if (this.milTime < currentHour) {
+    timeblockInput.addClass("past");
+  } else if (this.milTime === currentHour) {
+    timeblockInput.addClass("present");
+  } else if (this.milTime > currentHour) {
+    timeblockInput.addClass("future");
+  }
+  console.log(this.hour);
+  console.log(currentHour);
 });
